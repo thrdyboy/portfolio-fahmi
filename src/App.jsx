078274151f1beaa89a29023bb48d3d3bf9a03a96
unknown <tiansyahrocket@gmail.com>
2024-07-22 21:45:55 +0700

    firstPage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Example from './pages/contoh'
+
+function App() {
+  return (
+    <>
+      <Example />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/contoh.jsx
@@ -0,0 +1,178 @@
+function LandingPage() {
+  return (
+    <>
+      <div className="w-[1440px] h-[3984px] relative bg-amber-100">
+        <div className="left-[97px] top-[34px] absolute text-blue-950 text-2xl font-semibold font-['Poppins']">
+          Fahmi Rafif Tiansyah
+        </div>
+        <div className="left-[97px] top-[288px] absolute text-blue-950 text-[28px] font-medium font-['Poppins'] text-4xl fade-in">
+          Hello, I’m Fahmi,
+        </div>
+        <div className="left-[97px] top-[571px] absolute text-blue-950 text-[28px] font-medium font-['Poppins']">
+          based in Jakarta, Indonesia.
+        </div>
+        <div className="w-[900px] left-[97px] top-[1137px] absolute text-blue-950 text-2xl font-normal font-['Poppins'] leading-[44px]">
+          Hello Everyone, let me say it again Fahmi Rafif Tiansyah's here. And I want to show you about my Portfolio
+          as well. In my history I do on Cloud Computing and also doing some Front-end too while doing on final project
+          on My Major at Telkom University.
+        </div>
+        <div className="w-[900px] left-[97px] top-[2451px] absolute text-blue-950 text-2xl font-normal font-['Poppins'] leading-[44px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet
+          vulputate tristique quam felis. Id phasellus dui orci vulputate
+          consequat nulla proin. Id sit scelerisque neque, proin bibendum diam.
+        </div>
+        <div className="w-[640px] left-[596px] top-[3572px] absolute text-blue-950 text-2xl font-normal font-['Poppins'] leading-[44px]">
+          If u want to know me on what's going on around me, You can get my Contact on Social Media. With Social Media
+          that on the list below, you can stay on touch while i do a some project as well, Thank U.
+        </div>
+        <div className="left-[596px] top-[3769px] absolute text-blue-950 text-2xl font-normal font-['Poppins'] leading-[44px]">
+          Tiansyahrocket@outlook.com
+        </div>
+        <div className="left-[596px] top-[3813px] absolute text-blue-950 text-2xl font-normal font-['Poppins'] leading-[44px]">
+          twitter.com/thrdyaretweetin
+        </div>
+        <div className="left-[596px] top-[3857px] absolute text-blue-950 text-2xl font-normal font-['Poppins'] leading-[44px]">
+          instagram.com/thrdyboy
+        </div>
+        <div className="left-[97px] top-[3151px] absolute text-blue-950 text-2xl font-semibold font-['Poppins'] leading-[44px]">
+          Some Case Study
+        </div>
+        <div className="left-[777px] top-[3151px] absolute text-blue-950 text-2xl font-semibold font-['Poppins'] leading-[44px]">
+          Some Case Study
+        </div>
+        <div className="w-[560px] left-[97px] top-[3195px] absolute text-blue-950 text-lg font-normal font-['Poppins'] leading-loose">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
+          aliquam sollicitudin rhoncus morbi. Tincidunt quam sem elit a
+          convallis. Eget ipsum, velit vitae eu nunc, consequat, at.
+        </div>
+        <div className="w-[560px] left-[777px] top-[3195px] absolute text-blue-950 text-lg font-normal font-['Poppins'] leading-loose">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed
+          aliquam sollicitudin rhoncus morbi. Tincidunt quam sem elit a
+          convallis. Eget ipsum, velit vitae eu nunc, consequat, at.
+        </div>
+        <div className="left-[97px] top-[3107px] absolute text-blue-950 text-[15px] font-normal font-['Poppins'] leading-[44px]">
+          November 24, 2020
+        </div>
+        <div className="left-[777px] top-[3107px] absolute text-blue-950 text-[15px] font-normal font-['Poppins'] leading-[44px]">
+          November 24, 2020
+        </div>
+        <div className="w-[900px] h-[182px] left-[384px] top-[1414px] absolute">
+          <div className="w-[900px] left-0 top-[50px] absolute text-blue-950 text-2xl font-normal font-['Poppins'] leading-[44px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet
+            vulputate tristique quam felis. Id phasellus dui orci vulputate
+            consequat nulla proin. Id sit scelerisque neque, proin bibendum
+            diam.
+          </div>
+          <div className="left-0 top-0 absolute text-blue-950 text-2xl font-semibold font-['Poppins'] leading-10">
+            2014-2018
+          </div>
+        </div>
+        <div className="w-[900px] h-[182px] left-[384px] top-[1676px] absolute">
+          <div className="w-[900px] left-0 top-[50px] absolute text-blue-950 text-2xl font-normal font-['Poppins'] leading-[44px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet
+            vulputate tristique quam felis. Id phasellus dui orci vulputate
+            consequat nulla proin. Id sit scelerisque neque, proin bibendum
+            diam.
+          </div>
+          <div className="left-0 top-0 absolute text-blue-950 text-2xl font-semibold font-['Poppins'] leading-10">
+            2018-2020
+          </div>
+        </div>
+        <div className="w-[900px] h-[182px] left-[384px] top-[1938px] absolute">
+          <div className="w-[900px] left-0 top-[50px] absolute text-blue-950 text-2xl font-normal font-['Poppins'] leading-[44px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet
+            vulputate tristique quam felis. Id phasellus dui orci vulputate
+            consequat nulla proin. Id sit scelerisque neque, proin bibendum
+            diam.
+          </div>
+          <div className="left-0 top-0 absolute text-blue-950 text-2xl font-semibold font-['Poppins'] leading-10">
+            2020 - Present
+          </div>
+        </div>
+        <div className="w-[616px] left-[97px] top-[400px] absolute text-blue-950 text-[100px] font-extrabold font-['Poppins'] leading-[116px]">
+          Student
+        </div>
+        <div className="left-[97px] top-[989px] absolute text-yellow-200 text-[100px] font-extrabold font-['Poppins'] leading-[116px]">
+          about.
+        </div>
+        <div className="left-[97px] top-[2303px] absolute text-yellow-200 text-[100px] font-extrabold font-['Poppins'] leading-[116px]">
+          work.
+        </div>
+        <div className="left-[97px] top-[3415px] absolute text-yellow-200 text-[100px] font-extrabold font-['Poppins'] leading-[116px]">
+          contact.
+        </div>
+        <div className="w-[276px] h-[27px] left-[582px] top-[37px] absolute">
+          <div className="left-0 top-0 absolute text-blue-950 text-lg font-normal font-['Poppins']">
+            Home
+          </div>
+          <div className="left-[230px] top-0 absolute text-blue-950 text-lg font-normal font-['Poppins']">
+            Work
+          </div>
+          <div className="left-[115px] top-0 absolute text-blue-950 text-lg font-normal font-['Poppins']">
+            About
+          </div>
+        </div>
+        <div className="w-[26px] h-[16.10px] left-[1256px] top-[42px] absolute" />
+        <div className="w-[26px] h-[17.79px] left-[1311px] top-[41px] absolute" />
+        <div className="w-[26px] h-[15.05px] left-[1201px] top-[42px] absolute" />
+        <img
+          className="w-[440px] h-[314px] left-[97px] top-[3580px] absolute"
+          src=''
+        />
+        <div className="w-[506px] h-[509px] left-[831px] top-[191px] absolute">
+          <div className="w-[493px] h-[493px] left-[13px] top-0 absolute">
+            <img
+              className="w-[493px] h-[532px] left-0 top-0 absolute rounded-s-full"
+              src='./src/assets/Background-photo.jpg'
+            />
+          </div>
+        </div>
+        <div className="w-[103.85px] h-[29.44px] left-[823px] top-[564px] absolute">
+          <div className="w-[5px] h-[30px] left-[12.20px] top-0 absolute origin-top-left rotate-[24deg] bg-lime-900" />
+          <div className="w-[5px] h-[30px] left-[33.97px] top-0 absolute origin-top-left rotate-[24deg] bg-lime-900" />
+          <div className="w-[5px] h-[30px] left-[55.74px] top-0 absolute origin-top-left rotate-[24deg] bg-lime-900" />
+          <div className="w-[5px] h-[30px] left-[77.51px] top-0 absolute origin-top-left rotate-[24deg] bg-lime-900" />
+          <div className="w-[5px] h-[30px] left-[99.28px] top-0 absolute origin-top-left rotate-[24deg] bg-lime-900" />
+        </div>
+        <div className="w-[50px] h-14 left-[1246px] top-[174px] absolute">
+          <div className="w-6 h-6 left-0 top-[16px] absolute">
+            <div className="w-1.5 h-6 left-[9px] top-0 absolute bg-lime-900 rounded-md" />
+            <div className="w-1.5 h-6 left-0 top-[15px] absolute origin-top-left -rotate-90 bg-lime-900 rounded-md" />
+          </div>
+          <div className="w-6 h-6 left-[26px] top-[32px] absolute">
+            <div className="w-1.5 h-6 left-[9px] top-0 absolute bg-lime-900 rounded-md" />
+            <div className="w-1.5 h-6 left-0 top-[15px] absolute origin-top-left -rotate-90 bg-lime-900 rounded-md" />
+          </div>
+          <div className="w-6 h-6 left-[26px] top-0 absolute">
+            <div className="w-1.5 h-6 left-[9px] top-0 absolute bg-lime-900 rounded-md" />
+            <div className="w-1.5 h-6 left-0 top-[15px] absolute origin-top-left -rotate-90 bg-lime-900 rounded-md" />
+          </div>
+        </div>
+        <div className="w-[164px] h-[68px] left-[97px] top-[650px] absolute">
+          <div className="w-[158px] h-[61px] left-[6px] top-[7px] absolute bg-yellow-200 rounded-md" />
+          <div className="w-[158px] h-[61px] left-0 top-0 absolute rounded-md border border-lime-900" />
+          <button className="left-[38px] top-[16px] absolute text-lime-900 text-xl font-normal font-['Poppins']">
+            Resume
+          </button>
+        </div>
+        <div className="w-[560px] h-[400px] left-[97px] top-[2707px] absolute">
+          <img
+            className="w-[560px] h-[400px] left-0 top-0 absolute"
+            src="https://via.placeholder.com/560x400"
+          />
+        </div>
+        <div className="w-[560px] h-[400px] left-[777px] top-[2707px] absolute">
+          <img
+            className="w-[560px] h-[400px] left-0 top-0 absolute"
+            src="https://via.placeholder.com/560x400"
+          />
+        </div>
+        <div className="w-[13px] h-[13px] left-[353px] top-[1427px] absolute bg-blue-950 rounded-full" />
+        <div className="w-[13px] h-[13px] left-[353px] top-[1689px] absolute bg-blue-950 rounded-full" />
+        <div className="w-[13px] h-[13px] left-[353px] top-[1951px] absolute bg-blue-950 rounded-full" />
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
